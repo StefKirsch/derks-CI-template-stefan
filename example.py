398184e0cd3f0ade6daaf27b7d9bc25de103ed2b
Gijs Derks <g.l.derks@differ.nl>
2023-10-04 15:21:20 +0200

    added subtract

---
+++ example.py
@@ -0,0 +1,10 @@
+def sum(a,b):
+	return a+b
+
+def subtract(a,b)
+	return a+b
+
+def test_sum():
+	assert sum(2,3) == 5 # assert lets you verify IF (test) => TRUE
+	assert sum('space','ship') == 'spaceship' # also test string sums
+#pytest will search for test_functions and run them, returning outputs.
